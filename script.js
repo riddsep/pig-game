8287bb2bc330913e7f65cda544db36a79182e1ae
riddsep <ridoseptiawan00@gmail.com>
2024-11-04 19:05:17 +0700

    minor changes

--- script.js
+++ script.js
@@ -14,14 +14,25 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const score = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -53,13 +64,15 @@ btnRoll.addEventListener('click', () => {
 btnHold.addEventListener('click', () => {
   if (playing) {
     //Add current score to active player's score
-    score[activePlayer] += currentScore;
+    scores[activePlayer] += currentScore;
 
     document.getElementById(`score--${activePlayer}`).textContent =
-      score[activePlayer];
+      scores[activePlayer];
     //Check if player's score >= 100 finish game
-    if (score[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 10) {
       playing = false;
+      diceEl.classList.add('hidden');
+
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
