6a6368c7dc3e01cd56b21481638e7f3d288ee736
riddsep <ridoseptiawan00@gmail.com>
2024-11-04 16:14:07 +0700

    init project

---
+++ script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+//  Select elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const score = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+// Rolling dice functionality
+btnRoll.addEventListener('click', () => {
+  // Generate random dice number
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  // Display dice image
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // Check dice number: if 1, switch player
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch player
+    switchPlayer();
+  }
+});
+
+btnHold.addEventListener('click', () => {
+  //Add current score to active player's score
+  score[activePlayer] += currentScore;
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    score[activePlayer];
+  //Check if player's score >= 100 finish game
+
+  //Switch player
+  switchPlayer();
+});
