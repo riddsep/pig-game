1d23a807ffbfb5ebcd1a397e4f6a715cadc8f9d3
riddsep <ridoseptiawan00@gmail.com>
2024-11-05 20:04:55 +0700

    minor changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 'use strict';
 
 //  Select elements
-// const player0El = document.querySelector('.player--0');
-// const player1El = document.querySelector('.player--1');
-// const score0El = document.querySelector('#score--0');
-// const score1El = document.querySelector('#score--1');
-// const current0El = document.querySelector('#current--0');
-// const current1El = document.querySelector('#current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 
-// const diceEl = document.querySelector('.dice');
-// const btnNew = document.querySelector('.btn--new');
-// const btnRoll = document.querySelector('.btn--roll');
-// const btnHold = document.querySelector('.btn--hold');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 let scores, currentScore, activePlayer, playing;
